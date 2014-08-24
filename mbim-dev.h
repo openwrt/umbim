d16d0d2a487687ff38c3b7b5fc530d660b1d85cc
John Crispin <blogic@openwrt.org>
2014-08-24 20:47:21 +0200

    add gpl license
    Signed-off-by: John Crispin <blogic@openwrt.org>

--- mbim-dev.h
+++ mbim-dev.h
@@ -1,3 +1,17 @@
+/*
+ * umbim
+ * Copyright (C) 2014 John Crispin <blogic@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef _MBIM_DEV_H__
 #define _MBIM_DEV_H__
 
--- mbim-enum.c
+++ mbim-enum.c
@@ -1,3 +1,17 @@
+/*
+ * umbim
+ * Copyright (C) 2014 John Crispin <blogic@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <stdio.h>
 
 #include "mbim.h"
